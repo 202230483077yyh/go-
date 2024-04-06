613b310f542e07dc5153c0958a6f34c2a9502cac
202230483077yyh <3514715353@qq.com>
2024-04-06 16:38:01 +0800

    update

---
+++ 并发/goroutine.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//goroutine是协程，比线程更小
+
+func newTask() {
+	for {
+		fmt.Println("this is a newTask")
+		time.Sleep(time.Second)
+	}
+}
+
+// 注意：主协程退出，其他子协程也跟着退出
+func main() {
+	//只需在函数调用前添加go，新建一个任务（协程）
+	go newTask()
+	for {
+		fmt.Println("this is a main goroutine")
+		time.Sleep(time.Second)
+	}
+
+}
